bf78333d5fc1a4c3c8b4d0213a3f7518e89555d1
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2020-05-03 10:57:56 +0200

    Add translations

---
+++ translations/app/liri-player.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>BottomBar</name>
+    <message>
+        <location filename="../../src/app/qml/BottomBar.qml" line="169"/>
+        <location filename="../../src/app/qml/BottomBar.qml" line="173"/>
+        <source>Open a file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/BottomBar.qml" line="175"/>
+        <source>Opening...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/BottomBar.qml" line="177"/>
+        <source>Buffering...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/BottomBar.qml" line="182"/>
+        <source>%1 / %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LibraryDrawer</name>
+    <message>
+        <location filename="../../src/app/qml/LibraryDrawer.qml" line="17"/>
+        <source>Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/LibraryDrawer.qml" line="33"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/LibraryDrawer.qml" line="36"/>
+        <source>Videos</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistDrawer</name>
+    <message>
+        <location filename="../../src/app/qml/PlaylistDrawer.qml" line="21"/>
+        <source>Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/PlaylistDrawer.qml" line="72"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TopBar</name>
+    <message>
+        <location filename="../../src/app/qml/TopBar.qml" line="112"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/TopBar.qml" line="123"/>
+        <source>Open and add to queue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/TopBar.qml" line="131"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/app/qml/TopBar.qml" line="141"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
